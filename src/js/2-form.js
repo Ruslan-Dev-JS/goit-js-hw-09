6be44184e1ae3db1c81c8af6367253ad9fd4d2d3
RuslanLynnyk <ruslynnyk0001@gmail.com>
2025-11-18 08:08:39 +0200

    Added new HM

---
+++ src/js/2-form.js
@@ -0,0 +1,57 @@
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+const formData = {
+  email: "",
+  message: ""
+};
+
+
+const form = document.querySelector(".feedback-form");
+
+
+function loadFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+
+
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+
+  if (name in formData) {
+    formData[name] = value.trim(); 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData.email = "";
+  formData.message = "";
+});
+
+
+loadFormData();
